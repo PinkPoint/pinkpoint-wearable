daf25f5af3c1178c78fce6381a5decead1c624c6
Isabel Züger <iszu@zuehlke.com>
2015-04-13 17:27:20 +0200

    wip on write nfc tag

--- nfc_reader/readMifare_Demo_i2c/readMifare_Demo_i2c.ino
+++ nfc_reader/readMifare_Demo_i2c/readMifare_Demo_i2c.ino
@@ -91,7 +91,9 @@ void loop(void) {
   uint8_t success;
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
   uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
-    
+  uint8_t * routeName;
+  uint8_t * difficulty;
+  
   // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
   // 'uid' will be populated with the UID, and uidLength will indicate
   // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
@@ -135,104 +137,37 @@ void loop(void) {
         Serial.println(incomingByte);
         
         ReadPage(incomingByte);
+      } 
+      // WRITE 
+      else if(incomingByte == 0x57) {
+        Serial.print("you want to write a route. Please enter the route name (max 44 characters): ");
+        Serial.readBytesUntil(10, routeName, 44);
+
+        Serial.print("please enter the difficulty (max 4 characters): ");
+        Serial.readBytesUntil(10, difficulty, 4);
+
+        WritePage(routeName, difficulty);
       } else {
         Serial.println("nothing...");
       }
-    
-    
-    
-//    if (uidLength == 4)
-//    {
-//      // We probably have a Mifare Classic card ... 
-//      Serial.println("Seems to be a Mifare Classic card (4 byte UID)");
-//	  
-//      // Now we need to try to authenticate it for read/write access
-//      // Try with the factory default KeyA: 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF
-//      Serial.println("Trying to authenticate block 4 with default KEYA value");
-//      uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
-//	  
-//	  // Start with block 4 (the first block of sector 1) since sector 0
-//	  // contains the manufacturer data and it's probably better just
-//	  // to leave it alone unless you know what you're doing
-//      success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keya);
-//	  
-//      if (success)
-//      {
-//        Serial.println("Sector 1 (Blocks 4..7) has been authenticated");
-//        uint8_t data[16];
-//		
-//        // If you want to write something to block 4 to test with, uncomment
-//		// the following line and this text should be read back in a minute
-//        //memcpy(data, (const uint8_t[]){ 'a', 'd', 'a', 'f', 'r', 'u', 'i', 't', '.', 'c', 'o', 'm', 0, 0, 0, 0 }, sizeof data);
-//        // success = nfc.mifareclassic_WriteDataBlock (4, data);
-//
-//        // Try to read the contents of block 4
-//        success = nfc.mifareclassic_ReadDataBlock(4, data);
-//		
-//        if (success)
-//        {
-//          // Data seems to have been read ... spit it out
-//          Serial.println("Reading Block 4:");
-//          nfc.PrintHexChar(data, 16);
-//          Serial.println("");
-//		  
-//          // Wait a bit before reading the card again
-//          delay(1000);
-//        }
-//        else
-//        {
-//          Serial.println("Ooops ... unable to read the requested block.  Try another key?");
-//        }
-//      }
-//      else
-//      {
-//        Serial.println("Ooops ... authentication failed: Try another key?");
-//      }
-//    }
-    
-//    if (uidLength == 7)
-//    {
-//      // We probably have a Mifare Ultralight card ...
-//      Serial.println("Seems to be a Mifare Ultralight tag (7 byte UID)");
-//      Serial.println("Want do you want to do? C = Clear / R = Read");
-//     
-//      int incomingByte = 0;   // for incoming serial data
-//    
-//      while(!Serial.available());
-//
-//        // read the incoming byte:
-//        incomingByte = Serial.read();
-//
-//        // say what you got:
-//        //Serial.print("I received: ");
-//        //Serial.println(incomingByte, HEX);
-//        
-//        //CLEAR
-//        if(incomingByte == 0x43) {
-//          incomingByte = Serial.parseInt();
-//          
-//          Serial.print("you want to clear page ");
-//          Serial.println(incomingByte);
-//          
-//          mifareultralight_ClearPage(incomingByte);
-//        }
-//        // READ 
-//        else if(incomingByte == 0x52) {
-//          incomingByte = Serial.parseInt();
-//          
-//          Serial.print("you want to read page ");
-//          Serial.println(incomingByte);
-//          
-//          mifareultralight_ReadPage(incomingByte);
-//        } else {
-//          Serial.println("nothing...");
-//        }
-//    }
   }
 }
 
-void WritePage(uint8_t page, uint8_t * data) {
-  
+void WritePage(uint8_t * routeName, uint8_t * difficulty) {
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+  if (success) {
+    if (uidLength == 4) {
+      //mifareclassic_WritePage(page, data);
+    } 
+    else if(uidLength == 7) {      
+      mifareultralight_WritePage(routeName, difficulty);
+    }  
+  } else {
+    Serial.print("no tag detected");  
+  }
 }
 
 void ClearPage(uint8_t page) {
@@ -303,6 +238,41 @@ void mifareclassic_ClearPage(uint8_t page) {
   }
 }
 
+void mifareclassic_WritePage(uint8_t page, uint8_t * data) {
+  uint8_t success;
+  uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+  
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+  nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+  
+  // Now we need to try to authenticate it for read/write access
+  // Try with the factory default KeyA: 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF
+  success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, page - (page % 4), 0, keya);
+  if (success)
+  {
+    Serial.println("authenticated...");
+    	
+    success = nfc.mifareclassic_WriteDataBlock(page, data);
+    if (success)
+    {
+      Serial.print("wrote block ");  
+      Serial.print(page);
+      Serial.print(": ");
+      nfc.PrintHexChar(data, 4);
+    }
+    else
+    {
+      Serial.print("could not write block ");
+      Serial.println(page);
+    }
+  }
+  else
+  {
+    Serial.println("Ooops ... authentication failed: Try another key?");
+  }
+}
+
 
 void mifareclassic_ReadPage(uint8_t page) {
   uint8_t data[16];
@@ -353,6 +323,50 @@ void mifareultralight_ClearPage(uint8_t page) {
   }
 }
 
+void mifareultralight_WritePage(uint8_t * routeName, uint8_t * difficulty) {
+  uint8_t success;
+  int pageCount;
+  int arraySize;
+  int i;
+  int pageSize = 4;
+  int maxPageCount = 16;
+  int difficultyStartPage = 4;
+  int routeNameStartPage = 5;
+  
+  // write diffculty to page 4
+  success = nfc.mifareultralight_WritePage(difficultyStartPage, difficulty);
+  if(success) {
+    Serial.print("wrote difficulty ");
+    Serial.print(": ");
+    nfc.PrintHexChar(difficulty, pageSize);
+ } 
+  // write route name to page 5-15
+  arraySize = sizeof(routeName)/sizeof(routeName[0]);
+  
+  if(arraySize > 44) {
+      Serial.println("The route name contains more than 44 characters. Only 44 character will be written to the NFC tag.");
+  }
+  
+  pageCount = (arraySize / pageSize);
+  if(arraySize % pageSize > 0) {
+    pageCount++;
+  }
+
+  // write all characters of the route to the respecting pages. Break, if max number of pages exceeded  
+  success = 1;
+  for(i = routeNameStartPage; i < (pageCount + routeNameStartPage) && i < maxPageCount; i++){
+    success = success && nfc.mifareultralight_WritePage(i, &routeName[(i-routeNameStartPage) * pageSize]);    
+  }
+  
+  if(success) {
+    Serial.print("wrote route name ");
+    Serial.print(": ");
+    nfc.PrintHexChar(routeName, arraySize);
+  }  else {
+    Serial.print("could not write route name ");
+    nfc.PrintHexChar(routeName, arraySize);
+  }
+}
 
 void mifareultralight_ReadPage(uint8_t page) {
   uint8_t success;  
